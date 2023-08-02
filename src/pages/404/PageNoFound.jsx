bc6103f9fa02b26bae81e1ada27b66d6d8cb27fb
Tanjil Hossain <tanjilhossain845.com>
2023-08-02 09:31:05 +0600

    add 404 page

--- src/pages/404/PageNoFound.jsx
+++ src/pages/404/PageNoFound.jsx
@@ -1,9 +1,15 @@
-import './notFound.scss'
+import "./notFound.scss";
+import ContentWrapper from "../../components/contentWrapper/ContentWrapper"
 
 const PageNoTFound = () => {
   return (
-    <div>PageNoFound</div>
-  )
-}
+    <div className="pageNotFound">
+      <ContentWrapper>
+        <span className="bigText">404</span>
+        <span className="smallText">Page not found!</span>
+      </ContentWrapper>
+    </div>
+  );
+};
 
-export default PageNoTFound
+export default PageNoTFound;
